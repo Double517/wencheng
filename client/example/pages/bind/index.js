d66945ec964bbf9facfef4e9d6026821cce33f96
Jichao Wu <wujichao@gmail.com>
2016-04-29 16:53:10 +0800

    绑定页优化代码

--- client/example/pages/bind/index.js
+++ client/example/pages/bind/index.js
@@ -21,22 +21,21 @@ import Page from '../../component/page';
 export default class Bind extends React.Component {
     constructor (props) {
         super(props);
-
-        this.state = { submitting: false, errMsg: null};
     }
     componentDidMount() {
         this.loadWXConfig();
     }
     render() {
+        var isInputValid = this.isValidInput;
         return (
-            <Page className="cell" title="绑定">
+            <Page ref="page" className="cell" title="绑定">
                 <Form>
                     <FormCell>
                         <CellHeader>
                             <Label>账号</Label>
                         </CellHeader>
                         <CellBody>
-                        <Input onChange={this.onUsernameChange.bind(this)} ref="username" type="text" placeholder="请输入账号"/>
+                        <Input onChange={e=>this.onUsernameChange(e)} ref="username" type="text" placeholder="请输入账号"/>
                     </CellBody>
                     </FormCell>
                     <FormCell>
@@ -44,20 +43,19 @@ export default class Bind extends React.Component {
                             <Label>密码</Label>
                         </CellHeader>
                         <CellBody>
-                            <Input onChange={this.onPasswordChange.bind(this)} ref="password" type="password" placeholder="请输入密码"/>
+                            <Input onChange={e=>this.onPasswordChange(e)} ref="password" type="password" placeholder="请输入密码"/>
                         </CellBody>
                     </FormCell>
                 </Form>
 
                 <ButtonArea>
-                    <Button onClick={this.submit.bind(this)}>确定</Button>
-                    <Button type="default">取消</Button>
+                    <Button type={isInputValid?'primary':'default'}
+                            disabled={!isInputValid}
+                            onClick={e=>this.submit(e)}>
+                        确定
+                    </Button>
                 </ButtonArea>
-
-                <Toast icon="loading" show={this.state.submitting}>正在加载中...</Toast>
-                <Toast show={this.state.errMsg != null}>{this.state.errMsg}</Toast>
             </Page>
-
         );
     }
     // 拿到微信接口权限
@@ -75,7 +73,7 @@ export default class Bind extends React.Component {
                 wx.config(data);
             },
             error: function(xhr, type){
-                this.showToast('Ajax error!');
+                this.page.showError('Ajax error!');
             }
         });
     }
@@ -85,39 +83,49 @@ export default class Bind extends React.Component {
     onUsernameChange(event) {
         this.setState({username: event.target.value});
     }
-    submit() {
-        console.log(this.state);
+    get isValidInput() {
+        return this.state
+            && this.state.username
+            && this.state.username.length > 0
+            && this.state.password
+            && this.state.password.length > 0;
+    }
+    submit(event) {
 
-        this.setState({submitting: true});
+        // for debug
+        //wx.closeWindow();
 
+        if (!this.isValidInput) {
+            return;
+        }
+
+        this.page.showLoading();
         var data = JSON.stringify({ username: this.state.username, password: this.state.password, code: getParameterByName('code')});
         console.log(data);
-        wx.closeWindow();
         $.ajax({
             type: 'POST',
             url: '/api/bind',
             data: data,
             contentType: 'application/json',
             success: (data) => {
-                this.setState({submitting: false});
-
+                console.log(data);
                 if (data.code == 0) {
+                    this.page.showSuccess('绑定成功');
                     wx.closeWindow();
                 } else {
-                    this.showToast(data.msg);
+                    this.page.showError(data.msg);
                 }
             },
             error: (xhr, type) => {
-                this.showToast('Ajax error!');
+                console.log(type);
+                console.log(xhr);
+                this.page.showError('Ajax error!');
             }
         });
     }
 
-    showToast(errMsg) {
-        this.setState({errMsg: errMsg});
-        setTimeout(()=> {
-            this.setState({errMsg: null});
-        }, 1000);
+    get page() {
+        return this.refs.page;
     }
 };
 
