d23ce15741f1ea1c445c6a19d4c29bdb92dd16d0
Jichao Wu <wujichao@gmail.com>
2016-04-29 16:52:26 +0800

    加一些便捷的方法

--- client/example/component/page.js
+++ client/example/component/page.js
@@ -5,9 +5,24 @@
 "use strict";
 
 import React from 'react';
+import {Toast} from 'react-weui';
+
 import './page.less';
 
 export default class Page extends React.Component {
+
+    constructor (props) {
+        super(props);
+
+        this.state = {
+            toast: {show: false, icon:'', msg:''},
+        };
+        this.toastTimer = null;
+    }
+    componentWillUnmount() {
+        this.toastTimer && clearTimeout(this.toastTimer);
+    }
+
     render() {
         const {title, subTitle, spacing, className, children} = this.props;
 
@@ -20,7 +35,40 @@ export default class Page extends React.Component {
                 <div className={`bd ${spacing ? 'spacing' : ''}`}>
                     {children}
                 </div>
+                <Toast show={this.state.showToast}>完成</Toast>
+                <Toast icon={this.state.toast.icon}
+                       show={this.state.toast.show}>
+                    {this.state.toast.msg}
+                </Toast>
             </section>
         );
+    }
+
+    showToast(icon, msg, duration) {
+        if (this.toastTimer) {
+            clearTimeout(this.toastTimer);
+            this.toastTimer = null;
+        }
+
+        this.setState({toast:{icon:icon, show:true, msg:msg}});
+
+        if (duration) {
+            this.toastTimer = setTimeout(()=> {
+                this.hideToast();
+            }, duration * 1000);
+        }
+    }
+    hideToast() {
+        this.setState({toast:{show:false}});
+    }
+
+    showSuccess(msg) {
+        this.showToast('toast', msg?msg:'成功', 1.5);
+    }
+    showError(msg) {
+        this.showToast('success', msg?msg:'失败', 1.5);
+    }
+    showLoading(msg) {
+        this.showToast('loading', msg?msg:'加载中...');
     }
 };
--- client/example/pages/home/index.js
+++ client/example/pages/home/index.js
@@ -11,16 +11,44 @@ import {
     Button,ButtonArea
 } from 'react-weui';
 
+
 export default class Home extends React.Component {
 
     render() {
         return (
-            <Page className="home" title="WeUI" subTitle="为微信Web服务量身设计">
+            <Page ref="page" className="home" title="WeUI" subTitle="为微信Web服务量身设计">
                 <ButtonArea>
                     <Button>确定</Button>
                     <Button type="default">取消</Button>
+                    <Button onClick={(e)=>this.showSuccessToast(e)}>showSuccessToast</Button>
+                    <Button onClick={(e)=>this.showSuccessToast(e, 'success')}>showSuccessToast('success')</Button>
+                    <Button onClick={(e)=>this.showErrorToast(e)}>showErrorToast</Button>
+                    <Button onClick={(e)=>this.showErrorToast(e, 'error')}>showErrorToast('error')</Button>
+                    <Button onClick={(e)=>this.showLoading(e, 'loading')}>showLoading('loading')</Button>
+                    <Button onClick={(e)=>this.hideLoading(e)}>hideLoading</Button>
                 </ButtonArea>
             </Page>
         );
+    }
+
+    showSuccessToast(e, msg){
+        this.page.showSuccess(msg);
+    }
+    showErrorToast(e, msg){
+        this.page.showError(msg);
+    }
+    showLoading(e, msg) {
+        this.page.showLoading(msg);
+
+        setTimeout(()=> {
+            this.page.showSuccess('sss');
+        }, 2000);
+    }
+    hideLoading(e) {
+        this.page.hideToast();
+    }
+
+    get page() {
+        return this.refs.page;
     }
 };
